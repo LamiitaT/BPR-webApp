ce107f77c4792c19d638f0aab5e84fbd2d383c7a
LamiitaT <220394@via.dk>
2017-09-28 11:05:14 +0200

    master, login and logout pages

---
+++ WARDEN/WARDEN/Login.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WARDEN.Login" MasterPageFile ="~/Master.Master"%>
+
+<asp:Content ID="Content_Login" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id="login">
+                <table>
+                    <tr>
+                        <td>Username</td>
+                        <td>
+                            <asp:TextBox ID="user" runat="server"></asp:TextBox></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td>
+                            <asp:TextBox ID="pass" runat="server" TextMode="Password"></asp:TextBox></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <asp:Button ID="login_button" runat="server" Text="Login" OnClick="Login_Click" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <asp:Label ID="feedback" runat="server" Text=""></asp:Label>
+                        </td>
+                    </tr>
+                </table>
+    </div>
+    </asp:Content>
---
+++ WARDEN/WARDEN/Login.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WARDEN
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        MySqlConnection con = new MySqlConnection("Server=localhost;Database=warden;User id=root;");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session.Clear();
+        }
+
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            MySqlCommand cmd1;
+            cmd1 = new MySqlCommand("select * from login where username = '" + user.Text + "' and password =password('" + pass.Text + "')", con);
+            DataTable datatable1 = new DataTable();
+            MySqlDataAdapter adapter1 = new MySqlDataAdapter(cmd1);
+            adapter1.Fill(datatable1);
+            foreach (DataRow dr in datatable1.Rows)
+            {
+                Session["username"] = dr["username"].ToString();
+                Server.Transfer("Home.aspx");
+            }
+
+
+            con.Close();
+
+            feedback.Text = "Invalid username or password";
+
+        }
+    }
+}
